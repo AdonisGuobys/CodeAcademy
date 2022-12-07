7d6849a3770ce8d2bb84cd560e61b48c89a6c8c1
AdonisGuobys <adonis.guobys@gmail.com>
2022-12-07 11:48:31 +0200

    1

---
+++ Tasks/Task 1/main.py
@@ -0,0 +1,25 @@
+# Turimas "users" masyvas. 
+
+# Parašykite funkcijas, kurios atlikas nurodytas užduotis:
+# 1. funkcija "filterDogOwers" - kaip argumentą priims masyvą ir duoto masyvo 
+# atveju grąžins "users", kurie turi augintinį.
+# 2. funkcija "filterAdults" - kaip argumentą priims masyvą ir duoto masyvo 
+# atveju grąžins masyvą su "users", kurie yra pilnamečiai.
+
+# Write functions that will perform the given tasks:
+# 1. "filterDogOwers" function - as an argument in favor of masses and duet masses
+# will return "пользователи" who have a pet.
+# 2. "filterAdults" function - as an argument in favor of mass and duet mass
+# will return an array with "users" who are of legal age.
+
+users = 
+  { "id": '1', "name": 'John Smith', "age": 20, "hasDog": True },
+  { "id": '2', "name": 'Ann Smith', "age": 24, "hasDog": False },
+  { "id": '3', "name": 'Tom Jones', "age": 31, "hasDog": True },
+  { "id": '4', "name": 'Rose Peterson', "age": 17, "hasDog": False },
+  { "id": '5', "name": 'Alex John', "age": 25, "hasDog": True },
+  { "id": '6', "name": 'Ronald Jones', "age": 63, "hasDog": True },
+  { "id": '7', "name": 'Elton Smith', "age": 16, "hasDog": True },
+  { "id": '8', "name": 'Simon Peterson', "age": 30, "hasDog": False },
+  { "id": '9', "name": 'Daniel Cane', "age": 51, "hasDog": True },
+
