eaf3a96ef06d0d5be014c85cfddc3b7816c8c31c
AdonisGuobys <110552756+AdonisGuobys@users.noreply.github.com>
2023-04-11 13:21:07 +0300

    Fixed validation, added errors

--- forms.py
+++ forms.py
@@ -1,23 +1,43 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, EqualTo, ValidationError, Length, Regexp
+from models import User
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    username = StringField('Username', validators=[
+        DataRequired(),
+        Length(min=4, max=20, message="Username must be between 4 and 20 characters long."),
+        Regexp(r'^[a-zA-Z0-9_]+$', message="Username must contain only letters, numbers, or underscores.")
+    ])
+    password = PasswordField('Password', validators=[
+        DataRequired(),
+        Length(min=4, max=20, message="Password must be between 4 and 20 characters long.")
+    ])
+    confirm_password = PasswordField('Confirm Password', validators=[
+        DataRequired(),
+        EqualTo('password', message="Passwords must match.")
+    ])
     submit = SubmitField('Sign Up')
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('This username is already taken. Please choose a different one.')
+
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('Username', validators=[
+        DataRequired(message="Username is required."),
+        Length(min=4, max=20, message="Username must be between 4 and 20 characters long."),
+        Regexp(r'^[a-zA-Z0-9_]+$', message="Username must contain only letters, numbers, or underscores.")
+    ])
+    password = PasswordField('Password', validators=[
+        DataRequired(message="Password is required."),
+        Length(min=4, max=20, message="Password must be between 4 and 20 characters long.")
+    ])
     submit = SubmitField('Login')
 
-class CheckForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    amount = IntegerField('Amount', validators=[DataRequired()])
-    submit = SubmitField('Post')
+    def validate_password(self, password):
+        user = User.query.filter_by(username=self.username.data).first()
+        if not user or not user.check_password(password.data):
+            raise ValidationError('Username or password is incorrect. Please try again.')
 
-class GroupForm(FlaskForm):
-    group_name = StringField('Group Name', validators=[DataRequired()])
-    submit = SubmitField('Join/Create Group')
---
+++ models.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+import bcrypt
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def check_password(self, password):
+        return bcrypt.checkpw(password.encode(), self.password.encode())
